3a6a2f1fef20b60c512a848f2b777c36441a039f
AzurDelta <Rotar.zahar31@gmail.com>
2024-07-01 16:21:10 +0300

    json string from xml getter

---
+++ Pixycontroller.cs
@@ -0,0 +1,35 @@
+using System.Xml;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace pixy;
+[ApiController]
+[Route("pixy/crew")]
+public class Pixycontroller : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult> GetCrewList()
+    {
+        var url =
+            "http://api.pixelstarships.com/CharacterService/ListAllCharacterDesigns2?languageKey=en&version=0.999.5";
+        using (HttpClient client = new HttpClient())
+        {
+            HttpResponseMessage response = await client.GetAsync(url);
+            if (response.IsSuccessStatusCode)
+            {
+                string xmlString = await response.Content.ReadAsStringAsync();
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(xmlString);
+                var jsonString = JsonConvert.SerializeXmlNode(doc);
+                
+                return Ok(jsonString);
+                
+            }
+            else
+            {
+                throw new Exception($"Ошибка запроса: {response.StatusCode}");
+            }   
+        }
+    }
+}
