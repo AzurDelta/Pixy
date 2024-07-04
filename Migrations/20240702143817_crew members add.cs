07747ec7d0a28969d4ec2438533304b224cc9d2c
AzurDelta <Rotar.zahar31@gmail.com>
2024-07-04 17:52:17 +0300

    migrations added

---
+++ Migrations/20240702143817_crew members add.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace pixy.Migrations
+{
+    /// <inheritdoc />
+    public partial class crewmembersadd : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CrewMembers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CharacterHeadPartId = table.Column<int>(type: "integer", nullable: false),
+                    CharacterBodyPartId = table.Column<int>(type: "integer", nullable: false),
+                    CharacterLegPartId = table.Column<int>(type: "integer", nullable: false),
+                    Rarity = table.Column<string>(type: "text", nullable: false),
+                    SpecialAbilityType = table.Column<string>(type: "text", nullable: false),
+                    CollectionDesignId = table.Column<int>(type: "integer", nullable: false),
+                    EquipmentMask = table.Column<int>(type: "integer", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    CharacterDesignDescription = table.Column<string>(type: "text", nullable: false),
+                    Hp = table.Column<int>(type: "integer", nullable: false),
+                    FinalHp = table.Column<int>(type: "integer", nullable: false),
+                    BaseAttack = table.Column<double>(type: "double precision", nullable: false),
+                    FinalAttack = table.Column<double>(type: "double precision", nullable: false),
+                    BaseRepair = table.Column<double>(type: "double precision", nullable: false),
+                    FinalRepair = table.Column<double>(type: "double precision", nullable: false),
+                    SpecialAbilityBaseArgument = table.Column<double>(type: "double precision", nullable: false),
+                    SpecialAbilityFinalArgument = table.Column<double>(type: "double precision", nullable: false),
+                    BasePilot = table.Column<double>(type: "double precision", nullable: false),
+                    FinalPilot = table.Column<double>(type: "double precision", nullable: false),
+                    BaseScience = table.Column<double>(type: "double precision", nullable: false),
+                    FinalScience = table.Column<double>(type: "double precision", nullable: false),
+                    BaseWeapon = table.Column<double>(type: "double precision", nullable: false),
+                    FinalWeapon = table.Column<double>(type: "double precision", nullable: false),
+                    BaseEngine = table.Column<double>(type: "double precision", nullable: false),
+                    FinalEngine = table.Column<double>(type: "double precision", nullable: false),
+                    WalkingSpeed = table.Column<int>(type: "integer", nullable: false),
+                    RunSpeed = table.Column<int>(type: "integer", nullable: false),
+                    FireResistance = table.Column<int>(type: "integer", nullable: false),
+                    TrainingCapacity = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CrewMembers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Prestiges",
+                columns: table => new
+                {
+                    PrestigeID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    MemberOneId = table.Column<int>(type: "integer", nullable: false),
+                    MemberTwoId = table.Column<int>(type: "integer", nullable: false),
+                    ResultMemberId = table.Column<int>(type: "integer", nullable: false),
+                    PrestigeValue = table.Column<double>(type: "double precision", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Prestiges", x => x.PrestigeID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CrewMembers");
+
+            migrationBuilder.DropTable(
+                name: "Prestiges");
+        }
+    }
+}
---
+++ Migrations/20240704142050_separated tables.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace pixy.Migrations
+{
+    /// <inheritdoc />
+    public partial class separatedtables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EliteToUniques",
+                columns: table => new
+                {
+                    PrestigeId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    EliteOneId = table.Column<int>(type: "integer", nullable: false),
+                    EliteTwoId = table.Column<int>(type: "integer", nullable: false),
+                    ResultUniqueId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EliteToUniques", x => x.PrestigeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EpicToHeroes",
+                columns: table => new
+                {
+                    PrestigeId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    EpicOneId = table.Column<int>(type: "integer", nullable: false),
+                    EpicTwoId = table.Column<int>(type: "integer", nullable: false),
+                    ResultHeroId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EpicToHeroes", x => x.PrestigeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "HeroToLegs",
+                columns: table => new
+                {
+                    PrestigeId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    HeroOneID = table.Column<int>(type: "integer", nullable: false),
+                    HeroTwoID = table.Column<int>(type: "integer", nullable: false),
+                    ResultLegID = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HeroToLegs", x => x.PrestigeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UniqueToEpics",
+                columns: table => new
+                {
+                    PrestigeId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UniqueOneId = table.Column<int>(type: "integer", nullable: false),
+                    UniqueTwoId = table.Column<int>(type: "integer", nullable: false),
+                    ResultEpicId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UniqueToEpics", x => x.PrestigeId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EliteToUniques");
+
+            migrationBuilder.DropTable(
+                name: "EpicToHeroes");
+
+            migrationBuilder.DropTable(
+                name: "HeroToLegs");
+
+            migrationBuilder.DropTable(
+                name: "UniqueToEpics");
+        }
+    }
+}
